f03c73ccc641696ffc07a4f293998fd303cd728d
Brian Wawczak <bwawczak@students.mchenry.edu>
2018-08-30 09:41:50 -0500

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,19 @@
+public class main {
+
+    public static void main(String[] args){
+
+        int intNum1 = 29;
+        int intNum2 = 13;
+        int resultNum1 = intNum1 + intNum2;
+        int resultNum2 = intNum1 * intNum2;
+        double resultNum3 = (double)intNum1 / intNum2;
+        int resultNum4 = intNum1 % intNum2;
+        int resultNum5 = intNum1 - intNum2;
+
+        System.out.println("The sum of intNum1 and intNum2 is: " + resultNum1);
+        System.out.println("The product of intNum1 and intNum2 is: " + resultNum2);
+        System.out.println("The quotient of intNum1 and intNum2 is: " + resultNum3);
+        System.out.println("The remainder of intNum1 and intNum2 is: " + resultNum4);
+        System.out.println("The difference of intNum1 and intNum2 is: " + resultNum5);
+    }
+}
